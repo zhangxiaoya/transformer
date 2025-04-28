7888cd0f66a518539390b9010a730fd2f979ed3b
Ryan Cheung <ynzhang.vis@gmail.com>
2025-04-28 09:11:55 +0000

    [Added] constant definition

---
+++ constant/dataset.py
@@ -0,0 +1,18 @@
+# Dataset cache directory
+DATASET_CACHE_DIR = "/workspaces/dataset_cache"
+
+# Language pair
+SRC_LANGUAGE = "de"
+TGT_LANGUAGE = "en"
+
+# Dataset partitions
+TRAIN = "train"
+VALID = "valid"
+TEST = "test"
+
+# Define special symbols and indices
+PAD_IDX = 0
+UNK_IDX = 1
+BOS_IDX = 2
+EOS_IDX = 3
+
